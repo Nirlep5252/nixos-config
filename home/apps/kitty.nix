51119b72fc951ad0bc2b57925a0138243bbf7938
nirlep5252 <hello@nirlep.dev>
2024-11-24 21:50:01 +0530

    add kitty cursor trail

--- home/apps/kitty.nix
+++ home/apps/kitty.nix
@@ -12,6 +12,7 @@
       initial_window_height = 1080;
       window_padding_width = 6;
       background_opacity = "0.8";
+      cursor_trail = 10;
     };
     themeFile = "Catppuccin-Mocha";
   };
